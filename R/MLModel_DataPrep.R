7168dfcbc0cff3a6175b2d9e51ec1a98d64d1e55
matthewlouisdavisBioStat <67028278+matthewlouisdavisBioStat@users.noreply.github.com>
2022-01-16 13:21:46 -0600

    Fix code to get around stack error

--- R/MLModel_DataPrep.R
+++ R/MLModel_DataPrep.R
@@ -17,59 +17,59 @@
 #'  MLModel_DataPrep(fit,name = "new_fit",y = "outcome")
 #' }
 #' @export
-MLModel_DataPrep <- function(fit,name = "Default",y){
-  MLModel(
-    name = name,
-    response_types = c('BinomialVariate', 'factor', 'matrix', 'NegBinomialVariate', 'numeric', 'PoissonVariate','Surv'),
-    weights = F,
-    fit = function(formula, data, weights, ...) {
-       tr <- try(fit$mlmodel@x,silent = T)
-            if(class(tr) == 'try-error'){
-              tr <- try(fit$mlmodel@input,silent = T)
+MLModel_DataPrep <- function (fit, name = "Default", y) 
+{
+  MLModel(name = name, response_types = c("BinomialVariate", 
+                                          "factor", "matrix", "NegBinomialVariate", 
+                                          "numeric", "PoissonVariate", "Surv"), 
+          weights = F, fit = function(formula, data, weights, ...) {
+            tr <- try(fit$mlmodel@x, silent = T)
+            if (class(tr) == "try-error") {
+              tr <- try(fit$mlmodel@input, silent = T)
             }
-            if(class(tr) == 'try-error'){
-              stop('Cannot find recipe from fit object: try updating MachineShop to latest version, or using a recipe object to fit instead of a raw data set and formula separately')
+            if (class(tr) == "try-error") {
+              stop("Cannot find recipe from fit object: try updating MachineShop to latest version, or using a recipe object to fit instead of a raw data set and formula separately")
             }
-       dat <- tr %>% 
-        prep(training = data) %>%
-        bake(new_data = data)
-      mod <- fit
-      temp_dat <- dat
-      if(nrow(dat) == 1){
-        dat <- rbind(dat,dat)
-      }
-      fitNew <- fit(as.formula(paste0(y," ~ ",paste(colnames(dat)[!(colnames(dat) %in%
-                                    c(y,'(names)'))],collapse = " + "),collapse = "")),
-                    data = temp_dat,model = as.MLModel(mod))
-      fit <- lm(as.numeric(dat[[y]])[1:nrow(dat)] ~ 0 + predict(fitNew,newdata = dat,type = 'prob'))
-      fit$coefficients <- 1
-      fit$true_fit <- fitNew
-      fit$fit_mod <- mod
-      return(fit)
-    },
-    predict = function(object, newdata, ...) {
-      
-      mod <- object$fit_mod
-      tr <- try(mod$mlmodel@x,silent = T)
-            if(class(tr) == 'try-error'){
-              tr <- try(mod$mlmodel@input,silent = T)
+            dat <- tr %>% prep(training = data) %>% bake(new_data = data)
+            rm(tr)
+            mod <- fit
+            temp_dat <- dat
+            if (nrow(dat) == 1) {
+              dat <- rbind(dat, dat)
             }
-            if(class(tr) == 'try-error'){
-              stop('Cannot find recipe from fit object: try updating MachineShop to latest version, or using a recipe object to fit instead of a raw data set and formula separately')
+            temp_dat <- temp_dat[,c(y,
+                                    colnames(temp_dat)[!(colnames(temp_dat) %in% 
+                                                               c(y, "(names)"))])]
+            fo <- as.formula(paste0(y,"~ .",collapse = ""))
+            mlmodel <- as.MLModel(mod)
+            fitNew <- fit(fo, data = temp_dat, model = mlmodel)
+            print("Finished Fitting")
+            fit <- lm(as.numeric(dat[[y]])[1:nrow(dat)] ~ 0 + 
+                        predict(fitNew, newdata = dat, type = "prob"))
+            fit$coefficients <- 1
+            fit$true_fit <- fitNew
+            fit$fit_mod <- mod
+            return(fit)
+          }, predict = function(object, newdata, ...) {
+            mod <- object$fit_mod
+            tr <- try(mod$mlmodel@x, silent = T)
+            if (class(tr) == "try-error") {
+              tr <- try(mod$mlmodel@input, silent = T)
             }
-      dat <- tr %>%
-        prep %>%
-        bake(new_data = newdata)
-      if(nrow(dat) == 1){
-        predict(object$true_fit,newdata = rbind(dat,dat),type = 'prob')[1]
-      } else {
-        predict(object$true_fit,newdata = dat,type = 'prob')
-      }
-    },
-    varimp = function(object, ...) {
-      try(MachineShop::varimp(fit$true_fit,'model'),
-          silent = T)
-    }
-  )
+            if (class(tr) == "try-error") {
+              stop("Cannot find recipe from fit object: try updating MachineShop to latest version, or using a recipe object to fit instead of a raw data set and formula separately")
+            }
+            dat <- tr %>% prep %>% bake(new_data = newdata)
+            if (nrow(dat) == 1) {
+              predict(object$true_fit, newdata = rbind(dat, 
+                                                       dat), type = "prob")[1]
+            }
+            else {
+              predict(object$true_fit, newdata = dat, type = "prob")
+            }
+          }, varimp = function(object, ...) {
+            try(MachineShop::varimp(fit$true_fit, "model"), 
+                silent = T)
+          })
 }
 
